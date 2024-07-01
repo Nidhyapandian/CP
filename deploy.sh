68ba3cc3df2cb9805b837b49292b6cddf5af410d
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-01 19:18:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,46 @@
+#!/bin/bash
+
+# Check if the branch is dev
+if [ "$GIT_BRANCH" = "dev" ]; then
+    # Docker build and push
+    echo "Building Docker image..."
+    docker build -t smart24/dev:dev .
+    docker tag nginximage smart24/dev:dev
+    echo "Pushing Docker image to Docker Hub..."
+    docker push smart24/dev:dev
+    
+    echo "Docker image pushed successfully."
+    
+elif [ "$GIT_BRANCH" = "main" ]; then
+    # Docker build and push
+    echo "Building Docker image..."
+    docker build -t smart24/prod:prod .
+    docker tag nginximage smart24/prod:prod
+    echo "Pushing Docker image to Docker Hub..."
+    docker push smart24/prod:prod
+    
+    echo "Docker image pushed successfully."
+else
+    echo "Skipping Docker build and push."
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #! /bin/bash
 # GET CURRENT BRANCH:
 BRANCH_NAME =${BRANCH_NAME:-$( 'env.GIT_BRANCH.split('/').last()')}
