4a238f6f160f7aa8f18b994b5a38e551b2dee749
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-01 11:52:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 BRANCH_NAME=$(git rev-parse --abbrev -ref HEAD)
 
 
-if ["$BRANCH_NAME"="dev"]; then
+if ["$BRANCH_NAME" = "dev"]; then
 	./build.sh
 	DOCKER_REPO="smart24/myapp-dev"
 	docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
 	docker push $DOCKER_REPO:${BUILD_NUMBER}
 	docker push $DOCKER_REPO:latest
 
-elif ["$BRANCH_NAME"="prod"]; then
+elif ["$BRANCH_NAME" = "prod"]; then
 	./build.sh
 	DOCKER_REPO="smart24/myapp-prod"
 	docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
