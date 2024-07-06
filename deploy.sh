cbf8413d9a43c8e0bddb556fe707063c4e385103
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-06 16:54:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# Check if the branch is dev
-if [ "$GIT_BRANCH" == "dev" ]; then
-    ./build.sh
-    docker tag nginximage smart24/dev:dev
+if [ "${BRANCH_NAME}" -eq "dev" ]
+then
+
+	./build.sh
+ 	echo "haiiii dev  "
+      docker tag nginximage smart24/dev:dev
     echo "Pushing Docker image to Docker Hub..."
     docker push smart24/dev:dev
     
     echo "Docker image pushed successfully."
-    
-elif [ "$GIT_BRANCH" == "main" ]; then
-    ./build.sh   
-    docker tag nginximage smart24/prod:prod
+	
+
+elif [ "${BRANCH_NAME}" -eq "main" ]
+then
+	./build.sh
+ 	echo "mainnnnnnn"
+  docker tag nginximage smart24/prod:prod
     echo "Pushing Docker image to Docker Hub..."
     docker push smart24/prod:prod
-    
-    echo "Docker image pushed successfully."
-else
-    echo "Skipping Docker build and push."
+	
+else echo "Branch not configured for deployment" exit 1
 fi
+
 
 
 
